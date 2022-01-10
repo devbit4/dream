857a8fb5c19ee1c21c9140eb4a8b8ba8a8d36e7e
devbit4 <devbit4@gmail.com>
2022-01-10 17:31:33 +0900

    add time alert function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useInterval } from 'react-use';
+import Moment from 'react-moment';
 
 
 const Header = (props) => {
+    const [seconds, setSeconds] = useState(Date.now());
+    useInterval(() => {
+        setSeconds(Date.now());
+    }, 1000)
 
     return (
         <div className='header'>
             <header>
                 <i className="fas fa-rocket"></i>
-                <h1> bucketList</h1>
+                <h1> bucketlist</h1>
                 <span>{props.total}</span>
             </header>
+            <div className='time'>
+                <Moment format='YYYY/MM/DD HH:mm:ss'>{seconds}</Moment>
+
+            </div>
         </div>
     )
 };
